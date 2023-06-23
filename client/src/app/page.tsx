259333d35412e188ca5695dd963864760c0c27ae
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-06-23 19:57:16 +0200

    Components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,43 @@
+import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
-      Map
+    <main className="flex min-h-screen flex-col p-5 space-y-10">
+      <div className="flex space-x-1">
+        <Button variant="default">Button</Button>
+        <Button variant="default">Button</Button>
+      </div>
+
+      <Accordion type="multiple">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other
+            components&apos; aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It&apos;s animated by default, but you can disable it if you
+            prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
+
     </main>
   )
 }
