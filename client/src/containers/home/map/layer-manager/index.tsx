56709c885a507e0492e869c86308bfdef035f3bc
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-06-26 12:06:20 +0200

    Mapbox layer

--- client/src/containers/home/map/layer-manager/index.tsx
+++ client/src/containers/home/map/layer-manager/index.tsx
@@ -4,11 +4,51 @@ import { H3HexagonLayer, H3HexagonLayerProps } from '@deck.gl/geo-layers/typed';
 import { LineLayer, LineLayerProps } from '@deck.gl/layers/typed';
 
 import DeckLayer from '@/components/map/layers/deck-layer';
+import MapboxLayer from '@/components/map/layers/mapbox-layer';
 import { DeckMapboxOverlayProvider } from '@/components/map/provider';
 
 const LayerManager = () => {
   return (
     <DeckMapboxOverlayProvider>
+      <MapboxLayer
+        beforeId="building"
+        source={{
+          id: 'vector-mapbox-layer',
+          type: 'vector',
+          url: 'mapbox://layer-manager.1ecpue1k',
+        }}
+        styles={[
+          {
+            id: 'indicators',
+            'source-layer': 'Indicators',
+            type: 'fill',
+            paint: {
+              // fill color depends on a category value called bws_cat that goe from 0 to 4
+              'fill-color': [
+                'match',
+                ['get', 'bws_cat'],
+                0,
+                '#f7f7f7',
+                1,
+                '#d9f0a3',
+                2,
+                '#addd8e',
+                3,
+                '#78c679',
+                4,
+                '#31a354',
+                /* other */ '#ccc',
+              ],
+              'fill-opacity': 0.5,
+            },
+          },
+        ]}
+        settings={{
+          opacity: 1,
+          visibility: true,
+        }}
+      />
+
       <DeckLayer<LineLayerProps>
         id="line-layer"
         type={LineLayer}
