93b64577f0e8db7c4ca3fac36534d0eaf8fceffe
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-06-27 12:04:03 +0200

    Datasets sidebar

--- client/src/containers/home/datasets/index.tsx
+++ client/src/containers/home/datasets/index.tsx
@@ -1,10 +1,18 @@
 'use client';
 
+import { useState } from 'react';
+
 import { useGetDatasets } from '@serverless-app-scaffold/types/generated/dataset';
+import { ChevronLeft } from 'lucide-react';
 
 import env from '@/env.mjs';
 
+import { cn } from '@/lib/classnames';
+
+import { Button } from '@/components/ui/button';
+
 export default function Datasets() {
+  const [open, setOpen] = useState(true);
   const { data: datasetData } = useGetDatasets(
     {
       populate: '*',
@@ -16,13 +24,35 @@ export default function Datasets() {
     }
   );
 
-  console.info({
-    datasetData,
-  });
-
   return (
-    <main className="absolute">
-      <div className="h-screen w-screen"></div>
-    </main>
+    <div
+      className={cn({
+        'absolute left-0 top-0 flex h-full w-full max-w-[400px] flex-col bg-white transition-transform duration-500':
+          true,
+        'translate-x-0': open,
+        '-translate-x-full': !open,
+      })}
+    >
+      <div className="absolute left-full top-6 z-10">
+        <Button
+          variant="default"
+          size="icon"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          <ChevronLeft
+            className={cn({
+              'h-6 w-6 transition-transform': true,
+              'rotate-180': !open,
+            })}
+          />
+        </Button>
+      </div>
+
+      <div className="prose flex grow flex-col overflow-y-auto">
+        <h2>Datasets</h2>
+      </div>
+    </div>
   );
 }
