4f6d559d3a1c815b30d012804fa30d9185f44d49
Alvaro Leal <pursuance@gmail.com>
2023-08-29 13:30:16 +0200

    Allways add trailing comma

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,6 +3,7 @@ const tailwindPlugin = require('prettier-plugin-tailwindcss');
 const config = {
   semi: true,
   tabWidth: 2,
+  trailingComma: 'all',
   printWidth: 100,
   singleQuote: true,
   plugins: [tailwindPlugin],
--- client/next.config.mjs
+++ client/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   transpilePackages: ['@orcasa/types'],
   env: {
     RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED: 'false',
-  }
+  },
 };
 
 export default nextConfig;
