9f360d7c65fd4c703ca03552c74b5583cd4895a3
Alvaro Leal <pursuance@gmail.com>
2023-08-31 16:58:24 +0200

    Remove mapbox config style

--- client/src/constants/mapbox.ts
+++
@@ -1,8 +0,0 @@
-import env from '@/env.mjs';
-
-export const MAPBOX_STYLES = {
-  default:
-    env.NEXT_PUBLIC_ENVIRONMENT === 'production'
-      ? 'mapbox://styles/layer-manager/clj8fgofm000t01pjcu21agsd?fresh=true'
-      : 'mapbox://styles/layer-manager/clj8fgofm000t01pjcu21agsd?fresh=true',
-};
