cef160d17cbaaf03b9274654d20e62904c10d8da
Alvaro Leal <pursuance@gmail.com>
2023-09-04 15:28:52 +0200

    Remove unneded h3

--- client/src/containers/section/datasets/item.tsx
+++ client/src/containers/section/datasets/item.tsx
@@ -14,8 +14,6 @@ import {
 export default function DatasetsItem(props: Required<DatasetListResponseDataItem>) {
   return (
     <div>
-      <h3 className="text-base"></h3>
-
       <Accordion type="single" collapsible defaultValue={`${props.id}`} className="w-full">
         <AccordionItem key={props.id} value={`${props.id}`}>
           <AccordionTrigger className="text-base">{props.attributes.title}</AccordionTrigger>
