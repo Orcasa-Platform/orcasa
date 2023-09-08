e54a2636e45288244e9cbdf73b4b5baca7040dff
Alvaro <pursuance@gmail.com>
2023-09-08 11:04:34 +0200

    Update client/src/components/map/controls/constants.ts
    Co-authored-by: Clément Prod'homme <clementprdhomme@users.noreply.github.com>

--- client/src/components/map/controls/constants.ts
+++ client/src/components/map/controls/constants.ts
@@ -2,7 +2,7 @@ export const CONTROL_BUTTON_STYLES = {
   default:
     'relative flex items-center justify-center h-10 w-10 items-center bg-white p-1.5 text-black transition-colors',
   hover: 'hover:bg-slate-200',
-  active: 'active:bg-white active:outline active:outline-2 active:outline-white/50',
+  active: 'active:outline active:outline-2 active:outline-white/50',
   disabled: 'cursor-default opacity-50 pointer-events-none',
   dark: 'text-white bg-slate-700 border-slate-700 hover:bg-slate-600',
 };
