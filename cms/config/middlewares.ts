083ca7b5499b40f0464ad782d5164f9ab51a57b1
Tiago Garcia <tiagojsag@gmail.com>
2023-09-11 11:46:38 +0100

    Rename a few data fields; increase body size limit

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -5,7 +5,17 @@ export default [
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
-  'strapi::body',
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "6mb", // modify form body
+      jsonLimit: "6mb", // modify JSON body
+      textLimit: "6mb", // modify text body
+      formidable: {
+        maxFileSize: 6 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
