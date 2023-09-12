701699c93e548ada5171450da51771a4190d2070
Alvaro Leal <pursuance@gmail.com>
2023-09-12 13:43:00 +0200

    Change default basemap

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -21,7 +21,7 @@ import { urlSyncEffect } from 'recoil-sync';
 export const mapSettingsAtom = atom({
   key: 'map-settings',
   default: {
-    basemap: 'basemap-light',
+    basemap: 'basemap-satellite',
     labels: 'labels-dark',
   },
   effects: [
