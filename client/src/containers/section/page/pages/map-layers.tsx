a26e91efed9508dc2f051829a87013178ca3cfb6
Alvaro Leal <pursuance@gmail.com>
2023-09-13 12:29:26 +0200

    Restructure pages. Style buttons

---
+++ client/src/containers/section/page/pages/map-layers.tsx
@@ -0,0 +1,15 @@
+const MapLayersPage = () => {
+  return (
+    <>
+      <h1 className="font-serif text-3.5xl">
+        Identify <span className="font-semibold">areas of interest</span> for research or
+        interventions.
+      </h1>
+      <p>
+        Give context to your research visualising scientifically-reliable soil-related map layers.
+      </p>
+    </>
+  );
+};
+
+export default MapLayersPage;
---
+++ client/src/containers/section/page/pages/network.tsx
@@ -0,0 +1,37 @@
+import { Plus } from 'lucide-react';
+
+import NetworkList from '@/containers/section/page/networks/network-list';
+
+import { Button } from '@/components/ui/button';
+
+const NetworkPage = () => {
+  const AddButton = ({ text }: { text: string }) => (
+    <Button
+      onClick={() => {
+        // TODO - add elements
+      }}
+    >
+      <Plus className="mr-2 h-6 w-6" />
+      <div className="text-base">{text}</div>
+    </Button>
+  );
+  return (
+    <>
+      <h1 className="font-serif text-3.5xl">
+        Discover <span className="font-semibold">who does what</span> on soils carbon.
+      </h1>
+      <NetworkList />
+      <div className="flex items-center justify-between">
+        <div className="text-xl font-semibold leading-[30px]">
+          Help us building the soil-carbon network
+        </div>
+        <div className=" space-x-4">
+          <AddButton text="Add organisation" />
+          <AddButton text="Add project" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NetworkPage;
