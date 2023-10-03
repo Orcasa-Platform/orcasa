cc8a937f2d06007adc84146ace855d4357ba8ab2
Alvaro Leal <pursuance@gmail.com>
2023-10-03 15:57:51 +0200

    Update h1

--- client/src/containers/section/page/pages/geospatial-data.tsx
+++ client/src/containers/section/page/pages/geospatial-data.tsx
@@ -1,13 +1,13 @@
 const GeospatialDataPage = () => {
   return (
     <>
-      <h1 className="font-serif text-3.5xl">
-        Identify <span className="font-semibold">areas of interest</span> for research or
-        interventions.
+      <h1 className="max-w-[372px] border-l-4 border-yellow-400 pl-5 font-serif text-lg leading-7">
+        <span className="text-slate-700">
+          Give context to your research by scientifically-reliable{' '}
+        </span>
+        <span className="font-semibold text-yellow-400">soil-related geospatial data</span>
+        <span className="text-slate-700">.</span>
       </h1>
-      <p>
-        Give context to your research visualising scientifically-reliable soil-related map layers.
-      </p>
     </>
   );
 };
