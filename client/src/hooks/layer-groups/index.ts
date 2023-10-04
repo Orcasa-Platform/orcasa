7a5e86aa0acf109260bc1c4b6b3746ce8336476d
Alvaro Leal <pursuance@gmail.com>
2023-10-04 11:25:56 +0200

    Sort layer groups by order

--- client/src/hooks/layer-groups/index.ts
+++ client/src/hooks/layer-groups/index.ts
@@ -3,7 +3,11 @@
 import { useGetLayerGroups } from '@/types/generated/layer-group';
 
 export const useLayerGroups = (layerGroupIds: number[]) => {
-  const query = useGetLayerGroups({ populate: '*', filters: { id: { $in: layerGroupIds } } });
+  const query = useGetLayerGroups({
+    populate: '*',
+    sort: 'order:asc',
+    filters: { id: { $in: layerGroupIds } },
+  });
   return {
     ...query,
     groups: query.data?.data,
