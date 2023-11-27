78edb65a7e3b29e48cbeed9ea4f79667c9c248ba
Alvaro Leal <pursuance@gmail.com>
2023-11-27 15:44:20 +0100

    Add border top to nav links in home

--- client/src/constants/modules.ts
+++ client/src/constants/modules.ts
@@ -2,22 +2,27 @@ export const moduleColors = {
   brown: {
     background: 'bg-brown-500',
     border: 'border-brown-500',
+    hoverBorder: 'hover:border-brown-500',
   },
   yellow: {
     background: 'bg-yellow-500',
     border: 'border-yellow-500',
+    hoverBorder: 'hover:border-yellow-500',
   },
   teal: {
     background: 'bg-teal-500',
     border: 'border-teal-500',
+    hoverBorder: 'hover:border-teal-500',
   },
   blue: {
     background: 'bg-blue-500',
     border: 'border-blue-500',
+    hoverBorder: 'hover:border-blue-500',
   },
   purple: {
     background: 'bg-purple-700',
     border: 'border-purple-700',
+    hoverBorder: 'hover:border-purple-700',
   },
 };
 
