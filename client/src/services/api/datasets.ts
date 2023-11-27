9b1286173e337906e31c36fd0c7c8e49e6d02630
Clément Prod'homme <contact@clementprodhomme.fr>
2023-11-27 15:57:26 +0100

    Remove comment disabling an ESLint rule

--- client/src/services/api/datasets.ts
+++ client/src/services/api/datasets.ts
@@ -8,11 +8,9 @@ export const DatasetsAPI = <T>(config: AxiosRequestConfig): Promise<T> => {
   const source = Axios.CancelToken.source();
   const promise = AXIOS_INSTANCE({ ...config, cancelToken: source.token }).then(({ data }) => data);
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  promise.cancel = () => {
+  config.signal?.addEventListener?.('abort', () => {
     source.cancel('Query was cancelled by React Query');
-  };
+  });
 
   return promise;
 };
