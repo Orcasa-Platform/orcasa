4699c4397b4cf0c13ff547239b812ae154bb403b
Alvaro Leal <pursuance@gmail.com>
2023-12-01 12:57:55 +0100

    Add projects to form with validation

--- client/src/app/(modules)/network/(form)/new/organisation/page.tsx
+++ client/src/app/(modules)/network/(form)/new/organisation/page.tsx
@@ -2,19 +2,19 @@
 
 import { useState } from 'react';
 
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitHandler, useForm, useFieldArray, ControllerRenderProps } from 'react-hook-form';
 
 import { useRouter } from 'next/navigation';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { AxiosError } from 'axios';
-import { Check, CircleSlash, AlertCircle } from 'lucide-react';
+import { Check, CircleSlash, AlertCircle, Plus, Trash2 } from 'lucide-react';
 import { z } from 'zod';
 
 import { cn } from '@/lib/classnames';
 
 import { postOrganizations } from '@/types/generated/organization';
-import { OrganizationRequest } from '@/types/generated/strapi.schemas';
+import { OrganizationRequest, OrganizationRequestData } from '@/types/generated/strapi.schemas';
 
 import { useFormGetFields } from '@/hooks/networks/forms';
 
@@ -40,7 +40,7 @@ type ZodField =
 export type Field = {
   label: string;
   required?: boolean;
-  zod: ZodField | z.ZodEffects<z.ZodOptional<z.ZodString>, string | undefined, string | undefined>;
+  zod?: ZodField | z.ZodEffects<z.ZodOptional<z.ZodString>, string | undefined, string | undefined>;
   description?: string | React.ReactNode;
   type: 'text' | 'textarea' | 'select' | 'multiselect' | 'email';
   options?: { label: string; value: string }[];
@@ -48,20 +48,18 @@ export type Field = {
   placeholder?: string;
 };
 
+const projectRoleOptions = [
+  { label: 'Coordinator', value: 'lead_projects' },
+  { label: 'Partner', value: 'partner_projects' },
+  { label: 'Funder', value: 'funded_projects' },
+];
+
 export default function OrganisationForm() {
-  const { organizationTypes, organizationThemes, countries } = useFormGetFields() || {};
+  const { organizationTypes, organizationThemes, countries, projects } = useFormGetFields() || {};
   const OtherId = organizationTypes?.find((type) => type?.name === 'Other')?.id?.toString();
-  const hasData = organizationTypes && organizationThemes && countries;
+  const hasData = organizationTypes && organizationThemes && countries && projects;
   const [error, setError] = useState<AxiosError | undefined>();
   const fields: { [key: string]: Field } | undefined = hasData && {
-    user_email: {
-      label: 'Email',
-      required: true,
-      zod: z.string().nonempty('User email').email('Please, enter a valid email.').max(255, {
-        message: 'Email is limited to 255 characters.',
-      }),
-      type: 'email',
-    },
     name: {
       label: 'Organisation name',
       required: true,
@@ -92,7 +90,7 @@ export default function OrganisationForm() {
         })
         .optional()
         .superRefine((organizationTypeOther, refinementContext) => {
-          const organizationType = form.watch('organization_type');
+          const organizationType = watch('organization_type');
           if (
             organizationType === OtherId &&
             (typeof organizationTypeOther === 'undefined' || organizationTypeOther.length === 0)
@@ -187,19 +185,112 @@ export default function OrganisationForm() {
         </div>
       ),
     },
+    user_email: {
+      label: 'Email',
+      required: true,
+      zod: z.string().nonempty('User email').email('Please, enter a valid email.').max(255, {
+        message: 'Email is limited to 255 characters.',
+      }),
+      type: 'email',
+    },
   };
 
-  const formSchema = z.object(
-    (fields ? Object.keys(fields) : []).reduce((acc: { [key: string]: Field['zod'] }, field) => {
+  const projectFields: { [key: string]: Field } | undefined = hasData && {
+    project: {
+      label: 'Project',
+      // zod will be added from projectsArraySchema
+      type: 'select',
+      options: projects.map((type) => ({
+        label: type.name,
+        value: type.id.toString(),
+      })),
+    },
+    role: {
+      label: 'Role',
+      // zod will be added from projectsArraySchema
+      type: 'select',
+      options: projectRoleOptions,
+    },
+  };
+
+  const projectsArraySchema = z.array(
+    z.object({
+      project: z
+        .enum(projects?.map((type) => type?.id?.toString()) as [string, ...string[]])
+        .optional()
+        .superRefine((project, context) => {
+          const index = context.path[1];
+          const role = watch(`projects.${index}.role`);
+          if (!project && !!role) {
+            return context.addIssue({
+              code: z.ZodIssueCode.custom,
+              message: 'Please, select a project for the role.',
+            });
+          }
+          return context;
+        }),
+
+      role: z
+        .enum(projectRoleOptions?.map((role) => role.value) as [string, ...string[]])
+        .optional()
+        .superRefine((role, context) => {
+          const index = context.path[1];
+          const project = watch(`projects.${index}.project`);
+          if (!role && !!project) {
+            return context.addIssue({
+              code: z.ZodIssueCode.custom,
+              message: 'Please, select a role for the project.',
+            });
+          }
+          return context;
+        }),
+    }),
+  );
+
+  const formFieldsSchema = fields
+    ? Object.keys(fields).reduce((acc: { [key: string]: Field['zod'] }, field) => {
       if (fields) {
         acc[field] = fields[field].zod;
       }
       return acc;
-    }, {}),
-  );
+    }, {})
+    : {};
 
-  const form = useForm<z.infer<typeof formSchema>>({
+  const formSchema = z.object({
+    ...formFieldsSchema,
+    ...(fields ? { projects: projectsArraySchema } : {}),
+  });
+
+  type FormType = z.infer<typeof formSchema>;
+
+  const form = useForm<FormType>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      projects: [
+        {
+          project: undefined,
+          role: undefined,
+        },
+      ],
+    },
+  });
+  const { control, handleSubmit, watch } = form;
+
+  const {
+    append,
+    remove,
+    fields: projectFormFields,
+  } = useFieldArray<FormType>({
+    name: 'projects',
+    control,
+    rules: {
+      validate: (value) => {
+        if (value.length < 2) {
+          return 'More values please';
+        }
+        return true;
+      },
+    },
   });
 
   const router = useRouter();
@@ -208,13 +299,45 @@ export default function OrganisationForm() {
     return null;
   }
 
-  const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = (data) => {
+  type DataType = Omit<OrganizationRequestData, 'projects'> & {
+    projects: { project: string; role: string }[];
+  };
+  const onSubmit: SubmitHandler<FormType> = (data) => {
+    const typedData: DataType = data as unknown as DataType;
     const normalizedData = {
-      ...data,
-      url: !data.url || data.url.startsWith('http') ? data.url : `https://${data.url}`,
+      ...typedData,
+      url:
+        !typedData.url || typedData.url.startsWith('http')
+          ? typedData.url
+          : `https://${typedData.url}`,
     };
+
+    type ProjectRole = 'lead_projects' | 'partner_projects' | 'funded_projects';
+
+    const projects: Record<ProjectRole, string[] | undefined> = {
+      lead_projects: undefined,
+      partner_projects: undefined,
+      funded_projects: undefined,
+    };
+
+    (normalizedData.projects as { project: string; role: ProjectRole }[]).forEach(
+      ({ project: projectData, role: projectRole }) => {
+        if (projectData) {
+          projects[projectRole] = (projects[projectRole] || []).concat(projectData);
+        }
+      },
+    );
+
+    // clean projects attribute from normalizedData
+    delete (normalizedData as typeof data).projects;
+
+    const dataWithProjects = {
+      ...normalizedData,
+      ...projects,
+    };
+
     postOrganizations({
-      data: normalizedData,
+      data: dataWithProjects,
     } as unknown as OrganizationRequest)
       .then(() => {
         router.push(`/network/new/organisation/thank-you`);
@@ -225,42 +348,68 @@ export default function OrganisationForm() {
       });
   };
 
-  const renderField = (key: string) => {
-    const { label, required, type, options, placeholder, maxSize, description } = fields[key];
+  const renderField = ({
+    key,
+    projectNumber,
+    id,
+    index,
+  }: {
+    key: string;
+    projectNumber?: number;
+    id?: string;
+    index?: number;
+  }) => {
+    const isProject = projectNumber !== undefined;
+    const field = isProject ? (projectFields?.[key] as Field) : fields[key];
+    if (!isProject && !field) return null;
+    const { label, required, type, options, placeholder, maxSize, description } = field;
+    console.log(isProject ? `projects.${projectNumber}.${key}` : key);
     return (
       <FormField
-        key={key}
-        name={key}
+        key={id || key}
+        name={isProject ? `projects.${projectNumber}.${key}` : key}
         control={form.control}
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel className="flex justify-between">
-              <span>
-                {label}
-                {required && (
-                  <>
-                    {' '}
-                    <span className="text-red-700">*</span>
-                  </>
-                )}
-              </span>
-              {!required && <span className="text-sm text-gray-700">Optional</span>}
-            </FormLabel>
-            <FormControl>
-              <InputComponent
-                field={field}
-                type={type}
-                required={required}
-                options={options}
-                watch={form.watch}
-                maxSize={maxSize}
-                placeholder={placeholder}
-              />
-            </FormControl>
-            <FormDescription className="text-sm text-slate-500">{description}</FormDescription>
-            <FormMessage className="max-w-[632px]" />
-          </FormItem>
-        )}
+        render={(f) => {
+          const { field } = f;
+          return (
+            <FormItem>
+              <FormLabel className="flex justify-between">
+                <span>
+                  {label}
+                  {required && (
+                    <>
+                      {' '}
+                      <span className="text-red-700">*</span>
+                    </>
+                  )}
+                </span>
+                {!required && <span className="text-sm text-gray-700">Optional</span>}
+              </FormLabel>
+              <FormControl>
+                <InputComponent
+                  field={
+                    field as unknown as ControllerRenderProps<
+                      { [x: string]: string | undefined },
+                      string
+                    >
+                  }
+                  key={id}
+                  index={index}
+                  name={key}
+                  type={type}
+                  required={required}
+                  options={options}
+                  watch={watch}
+                  maxSize={maxSize}
+                  placeholder={placeholder}
+                  register={form.register}
+                />
+              </FormControl>
+              <FormDescription className="text-sm text-slate-500">{description}</FormDescription>
+              <FormMessage className="max-w-[632px]" />
+            </FormItem>
+          );
+        }}
       />
     );
   };
@@ -271,7 +420,7 @@ export default function OrganisationForm() {
         <form
           noValidate
           className="min-w-[632px] max-w-[632px] pb-10"
-          onSubmit={form.handleSubmit(onSubmit)}
+          onSubmit={handleSubmit(onSubmit)}
         >
           <div className="fixed top-0 z-30 -ml-1 w-[calc(632px+8px)] bg-white px-1">
             <div className="mb-2 flex items-center justify-between border-b border-dashed border-gray-300  pb-6 pt-20">
@@ -306,15 +455,12 @@ export default function OrganisationForm() {
               <span> are mandatory.</span>
             </div>
             {Object.keys(fields)
-              .filter((f) => f !== 'user_email')
+              .filter((key) => key !== 'user_email' && !key.startsWith('project'))
               .map((key) => {
-                if (
-                  key === 'organization_type_other' &&
-                  form.watch('organization_type') !== OtherId
-                ) {
+                if (key === 'organization_type_other' && watch('organization_type') !== OtherId) {
                   return null;
                 }
-                return renderField(key);
+                return renderField({ key });
               })}
             <h2 className="mt-10 font-serif text-2xl text-gray-700">Organisation network</h2>
             <div className="flex w-full gap-3 rounded-md bg-peach-50 p-4">
@@ -324,7 +470,36 @@ export default function OrganisationForm() {
                 after you submit this organisation form.
               </div>
             </div>
-            <div>Network Fields</div>
+            {projectFormFields.map((project, index) => {
+              const { id } = project;
+              return (
+                <div className="mt-10" key={`project-${id}`}>
+                  {renderField({ key: 'project', projectNumber: index, id })}
+                  {renderField({ key: 'role', projectNumber: index, id })}
+
+                  <Button
+                    type="button"
+                    onClick={() => remove(index)}
+                    variant="secondary"
+                    className="flex gap-4"
+                  >
+                    <Trash2 className="h-6 w-6" />
+                    Remove project
+                  </Button>
+                </div>
+              );
+            })}
+            <div className="flex w-full justify-end">
+              <Button
+                type="button"
+                onClick={() => append({ project: undefined, role: undefined })}
+                variant="secondary"
+                className="gap-4"
+              >
+                <Plus className="h-6 w-6" />
+                Add another project
+              </Button>
+            </div>
             <div className="space-y-6 border-t border-dashed border-gray-300">
               <h2 className="mt-6 font-serif text-2xl text-gray-700">Contact Information</h2>
               <div className="text-gray-700">
@@ -332,7 +507,7 @@ export default function OrganisationForm() {
                 could require clarifications from our team, please enter your email so we can
                 contact you about it.
               </div>
-              {renderField('user_email')}
+              {renderField({ key: 'user_email' })}
             </div>
           </div>
         </form>
