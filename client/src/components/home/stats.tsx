75acd94a7150ed65f96351ddd950c8da323ddab5
Alvaro Leal <pursuance@gmail.com>
2023-12-13 09:36:20 +0100

    Add counter for the homepage stats

--- client/src/components/home/stats.tsx
+++ client/src/components/home/stats.tsx
@@ -2,6 +2,8 @@ import { cn } from '@/lib/classnames';
 
 import { getHomeStats } from '@/types/generated/home-stat';
 
+import Counter from './counter';
+
 export default async function Stats({ className }: { className: string }) {
   const data = await getHomeStats({ populate: '*' });
   const stats = data?.data?.map((item) => ({
@@ -18,7 +20,9 @@ export default async function Stats({ className }: { className: string }) {
           key={title}
           className="flex w-[110px] flex-col items-center justify-center gap-2 border border-l-gray-50 bg-white px-3 py-3 text-gray-500"
         >
-          <div className="text-center font-serif text-xl">{value}</div>
+          <div className="min-h-[28px] text-center font-serif text-xl">
+            {value && <Counter value={value} />}
+          </div>
           <div className={cn('h-1 w-4', itemClassName)} />
           <div className="text-center text-xs font-normal">{title}</div>
         </div>
