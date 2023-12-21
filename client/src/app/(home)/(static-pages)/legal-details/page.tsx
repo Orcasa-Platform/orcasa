45e42240bde300e6107e4b4ae12ff479bd32a5ce
Alvaro Leal <pursuance@gmail.com>
2023-12-21 14:50:37 +0100

    Use revalidate 0 on pages to force dynamic

--- client/src/app/(home)/(static-pages)/legal-details/page.tsx
+++ client/src/app/(home)/(static-pages)/legal-details/page.tsx
@@ -12,3 +12,5 @@ export const metadata: Metadata = {
   description:
     'Learn about ORCaSa, a Horizon Europe initiative that unites international stakeholders to develop techniques for capturing and storing carbon in soil. Join the effort to combat climate change today.',
 };
+
+export const revalidate = 0;
