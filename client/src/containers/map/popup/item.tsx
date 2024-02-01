150507a71d0c7a17abf8129a576f984b002f4a3d
Alvaro Leal <pursuance@gmail.com>
2024-02-01 17:39:04 +0100

    Add No data text for missing values. Remove warning for classname props

--- client/src/containers/map/popup/item.tsx
+++ client/src/containers/map/popup/item.tsx
@@ -123,6 +123,7 @@ const PopupItem = ({ id }: PopupItemProps) => {
       map?.off('render', handleMapRender);
     };
   }, [map, handleMapRender]);
+  const noData = <div className="text-xs">has No data</div>;
   return (
     <div className="space-y-3 pt-2 text-gray-700 first:pt-0">
       <ContentLoader
@@ -133,35 +134,42 @@ const PopupItem = ({ id }: PopupItemProps) => {
         isPlaceholderData={isPlaceholderData}
         skeletonClassName="h-20 w-[250px]"
       >
-        <div>
-          At the Coordinates:{' '}
-          <span className="text-xs font-semibold">
-            {popup?.lngLat.lng.toFixed(4)}, {popup?.lngLat.lat.toFixed(4)}
-          </span>
-        </div>{' '}
-        the <span className="font-semibold">{attributes.title}</span>
-        <dl className="flex items-center space-x-1">
-          {click && !!featuresData && (
-            <>
-              <span>is </span>
-              {click.values.map((v) => {
-                return (
-                  <div key={v.key}>
-                    <dd className="text-xs font-semibold">
-                      {format({
-                        id: v.format?.id,
-                        value: v.type === 'number' ? +featuresData[v.key] : featuresData[v.key],
-                        options: v.format?.options,
-                      })}{' '}
-                      {v.unit}
-                    </dd>
-                  </div>
-                );
-              })}
-            </>
-          )}
-          {click && !featuresData && <div className="text-xs">has No data</div>}
-        </dl>
+        {click && !!featuresData && click.values.some((v) => featuresData[v.key]) && (
+          <>
+            <div>
+              At the Coordinates:{' '}
+              <span className="text-xs font-semibold">
+                {popup?.lngLat.lng.toFixed(4)}, {popup?.lngLat.lat.toFixed(4)}
+              </span>
+            </div>{' '}
+            the <span className="font-semibold">{attributes.title}</span>
+            <dl className="flex items-center space-x-1">
+              {click && !!featuresData && click.values.some((v) => featuresData[v.key]) && (
+                <>
+                  {click.values.map((v) => {
+                    const value = format({
+                      id: v.format?.id,
+                      value: v.type === 'number' ? +featuresData[v.key] : featuresData[v.key],
+                      options: v.format?.options,
+                    });
+                    if (!value) return noData;
+                    return (
+                      <div key={v.key}>
+                        <dd className="text-xs">
+                          <span>is </span>
+                          <span className="font-semibold">
+                            {value} {v.unit}
+                          </span>
+                        </dd>
+                      </div>
+                    );
+                  })}
+                </>
+              )}
+              {click && !featuresData && noData}
+            </dl>
+          </>
+        )}
       </ContentLoader>
     </div>
   );
