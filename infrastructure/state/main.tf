8a15efe48ec2a57f74bd42f835d8b41ddf0d3b09
Tiago Garcia <tiagojsag@protonmail.com>
2024-02-14 10:24:44 +0100

    Add GH configurable env vars and secrets

--- infrastructure/state/main.tf
+++ infrastructure/state/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.14"
+      version = "~> 5.36"
     }
   }
 
-  required_version = "~> 1.5.6"
+  required_version = "~> 1.7.3"
 }
 
 provider "aws" {
