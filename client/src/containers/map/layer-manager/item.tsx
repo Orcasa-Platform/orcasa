e2b5cb0f1d16cb4bff605fc41c30d18ead289999
Alvaro Leal <pursuance@gmail.com>
2024-02-23 16:02:02 +0100

    Remove visual bounds. Add zoom on enable layer

--- client/src/containers/map/layer-manager/item.tsx
+++ client/src/containers/map/layer-manager/item.tsx
@@ -3,6 +3,7 @@
 import { useCallback } from 'react';
 
 import { Layer } from '@deck.gl/core/typed';
+import { LngLatBoundsLike, useMap } from 'react-map-gl/maplibre';
 
 import { parseConfig, JSON_CONFIGURATION } from '@/lib/json-converter';
 
@@ -12,7 +13,6 @@ import { useGetLayersId } from '@/types/generated/layer';
 import { LayerResponseDataObject } from '@/types/generated/strapi.schemas';
 import { Config, LayerTyped } from '@/types/layers';
 
-import BoundsLayer from '@/components/map/layers/bounds-layer';
 import DeckGLLayer from '@/components/map/layers/deckgl-layer';
 import MapboxLayer from '@/components/map/layers/mapbox-layer';
 
@@ -25,6 +25,7 @@ const LayerManagerItem = ({ id, beforeId, settings }: LayerManagerItemProps) => 
   const { data } = useGetLayersId(id, {
     populate: 'metadata',
   });
+  const { default: mapRef } = useMap();
   const [layersInteractive, setLayersInteractive] = useLayersInteractive();
   const [, setLayersInteractiveIds] = useLayersInteractiveIds();
 
@@ -32,12 +33,18 @@ const LayerManagerItem = ({ id, beforeId, settings }: LayerManagerItemProps) => 
     ({ styles }: Config) => {
       if (!data?.data?.attributes) return null;
 
-      const { interaction_config } = data.data.attributes as LayerTyped;
+      const { interaction_config, highlighted_bounds } = data.data.attributes as LayerTyped;
       if (interaction_config?.enabled) {
         const ids = styles.map((l) => l.id);
         // We use Set here as there were some race conditions on layers with tabs on the toggling of interactive layers
         setLayersInteractive((prev) => [...new Set([...prev, id])]);
         setLayersInteractiveIds((prev) => [...new Set([...prev, ...ids.map((id) => +id)])]);
+      }
+
+      const bounds = highlighted_bounds as number[][] | null;
+      if (bounds && mapRef) {
+        const bbox = bounds as LngLatBoundsLike;
+        mapRef.fitBounds(bbox);
       }
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -65,7 +72,7 @@ const LayerManagerItem = ({ id, beforeId, settings }: LayerManagerItemProps) => 
   const { type } = data.data.attributes as LayerTyped;
 
   if (type === 'mapbox') {
-    const { config, params_config, highlighted_bounds } = data.data.attributes;
+    const { config, params_config } = data.data.attributes;
 
     const c = parseConfig<Config>({
       config,
@@ -74,11 +81,10 @@ const LayerManagerItem = ({ id, beforeId, settings }: LayerManagerItemProps) => 
       jsonConfiguration: JSON_CONFIGURATION,
     });
 
-    const bounds = highlighted_bounds as number[][] | null;
-
     if (!c) return null;
     // We need to add a key when we replace the tiles only for raster layers as it will fail on source change
     const keyProp = c.source.type === 'raster' ? { key: c.source.tiles?.toString() } : {};
+
     return (
       <>
         <MapboxLayer
@@ -89,9 +95,6 @@ const LayerManagerItem = ({ id, beforeId, settings }: LayerManagerItemProps) => 
           onAdd={handleAddMapboxLayer}
           onRemove={handleRemoveMapboxLayer}
         />
-        {bounds && bounds.length > 0 && (
-          <BoundsLayer id={`${id}-bounds-layer`} config={c} bounds={bounds} beforeId={beforeId} />
-        )}
       </>
     );
   }
