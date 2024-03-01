bbd0dce9eb65480d8d3778c6dcb19f765e3e3413
Alvaro Leal <pursuance@gmail.com>
2024-03-01 11:25:21 +0100

    Standarize tooltip withEllipsis and make accesible with keyboard

--- client/src/components/ui/with-ellipsis.tsx
+++ client/src/components/ui/with-ellipsis.tsx
@@ -1,31 +1,37 @@
 import { useState } from 'react';
 
-import { Tooltip, TooltipContent, TooltipTrigger, TooltipArrow } from '@radix-ui/react-tooltip';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipArrow,
+  TooltipProvider,
+} from '@/components/ui/tooltip';
 
 const WithEllipsis = ({ text, maxLength = 35 }: { text: string; maxLength?: number }) => {
   const [tooltipOpen, setTooltipOpen] = useState(false);
+  const handleTooltipClick = () => setTooltipOpen((prevOpen) => !prevOpen);
 
   if (text?.length < maxLength) return text;
 
   const triggerText = text && `${text.slice(0, maxLength)}...`;
   return (
-    <Tooltip open={tooltipOpen}>
-      <TooltipTrigger
-        className="cursor-default"
-        onMouseEnter={() => setTooltipOpen(true)}
-        onMouseLeave={() => setTooltipOpen(false)}
-      >
-        {triggerText}
-      </TooltipTrigger>
-      <TooltipContent
-        align="center"
-        side="bottom"
-        className="z-50 max-w-[300px] bg-white p-1 text-center text-xs text-slate-700"
-      >
-        {text}
-        <TooltipArrow className="z-50 fill-secondary" width={10} height={5} />
-      </TooltipContent>
-    </Tooltip>
+    <TooltipProvider delayDuration={0}>
+      <Tooltip open={tooltipOpen}>
+        <TooltipTrigger
+          className="cursor-default"
+          onMouseEnter={() => setTooltipOpen(true)}
+          onMouseLeave={() => setTooltipOpen(false)}
+          onClick={handleTooltipClick}
+        >
+          {triggerText}
+        </TooltipTrigger>
+        <TooltipContent className="text-sm leading-7" variant="dark">
+          {text}
+          <TooltipArrow variant="dark" />
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
