5df71130f476e6d9c5d42e4738b900c70443a5f3
Clément Prod'homme <contact@clementprodhomme.fr>
2024-03-19 10:12:25 +0100

    Fix linting and compilation error

--- client/src/pages/api/robots.ts
+++ client/src/pages/api/robots.ts
@@ -1,14 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+
 import env from '@/env.mjs';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<String>) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<string>) {
   if (env.NEXT_USE_RESTRICTIVE_ROBOTS_TXT === 'true') {
-    res.status(200).send(
-      `User-agent: *\nDisallow: /`,
-    );
+    res.status(200).send(`User-agent: *\nDisallow: /`);
   } else {
-    res.status(200).send(
-      `User-agent: *\nDisallow: /cms/`,
-    );
+    res.status(200).send(`User-agent: *\nDisallow: /cms/`);
   }
 }
