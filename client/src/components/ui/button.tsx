6eccaa1f6a0ff377bff300aa908824c1a09f6dcc
Clément Prod'homme <contact@clementprodhomme.fr>
2024-03-25 10:36:47 +0100

    Update the styles of the recommend section

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -6,7 +6,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/classnames';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center text-base lg:text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-green-700 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 font-sans',
+  'inline-flex items-center justify-center text-base ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-green-700 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 font-sans',
   {
     variants: {
       variant: {
@@ -14,8 +14,7 @@ const buttonVariants = cva(
         default: 'bg-gray-50 hover:bg-gray-150 rounded-lg',
         primary: 'bg-gray-700 hover:bg-gray-800 text-white',
         vanilla: '',
-        outline:
-          'border border-gray-700 px-4 py-2 hover:bg-gray-50 text-gray-700 hover:bg-slate-700 hover:text-white',
+        outline: 'border border-gray-300 rounded-lg px-6 py-4 hover:bg-gray-150 text-gray-700',
         // Not reviewed yet
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-gray-50',
