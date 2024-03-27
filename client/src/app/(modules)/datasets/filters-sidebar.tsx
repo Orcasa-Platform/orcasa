31aafe77be71d659886ab5916e79fcc4c55466cb
Alvaro Leal <pursuance@gmail.com>
2024-03-27 18:43:57 +0100

    Style selectors. Start styling calendar

--- client/src/app/(modules)/datasets/filters-sidebar.tsx
+++ client/src/app/(modules)/datasets/filters-sidebar.tsx
@@ -8,7 +8,7 @@ import { ChevronDown } from 'lucide-react';
 import { cn } from '@/lib/classnames';
 import { format } from '@/lib/utils/formats';
 
-import { useDatasetsFilters, useFiltersCount } from '@/store/datasets';
+import { useDatasetsFilters } from '@/store/datasets';
 
 import { DatasetSource } from '@/types/datasets';
 
@@ -16,6 +16,8 @@ import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
 import { MultiCombobox } from '@/components/ui/multi-combobox';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+
+import Reset from '@/styles/icons/reset.svg';
 
 export default function FiltersSidebar({
   filterSidebarOpen,
@@ -25,7 +27,6 @@ export default function FiltersSidebar({
   setFilterSidebarOpen: (open: boolean) => void;
 }) {
   const [filters, setFilters] = useDatasetsFilters();
-  const filtersCount = useFiltersCount(['search']);
 
   const scrollableContainerRef = useRef<HTMLDivElement | null>(null);
   const closeButtonRef = useRef<HTMLButtonElement | null>(null);
@@ -49,14 +50,11 @@ export default function FiltersSidebar({
       // `inert` is not yet supported by React so that's why it is spread below:
       // https://github.com/facebook/react/issues/17157
       {...(!filterSidebarOpen ? { inert: '' } : {})}
-      className={cn('ml-6 h-full border-l border-gray-300 bg-gray-700 duration-500', {
-        'w-0 shrink': !filterSidebarOpen,
-        'w-[380px]': filterSidebarOpen,
-      })}
+      className={cn('ml-6 h-full w-[275px] transform border-l border-gray-300 bg-gray-700')}
     >
       <div
         ref={scrollableContainerRef}
-        className="relative flex h-full flex-col gap-y-10 overflow-y-auto p-10 pt-4"
+        className="relative flex h-full flex-col gap-y-10 overflow-y-auto p-10 pt-4 text-white"
       >
         <Button
           ref={closeButtonRef}
@@ -69,15 +67,12 @@ export default function FiltersSidebar({
           <X className="h-4 w-4" />
         </Button>
         <h2 className="mb-6 font-serif text-2xl text-yellow-500">Filters</h2>
-        <div className="flex flex-col gap-y-10">
+        <div className="relative flex flex-col gap-y-10">
           <fieldset>
-            <legend className="mb-6 font-serif text-3.8xl">Filters</legend>
             <Button
               type="button"
-              variant="vanilla"
-              size="auto"
-              className="text-base font-semibold text-purple-700 hover:text-purple-900 disabled:text-gray-300 disabled:opacity-100"
-              disabled={filtersCount === 0}
+              size="xs"
+              className="absolute bottom-full left-0 flex -translate-y-4 items-center gap-1 rounded-2xl text-sm text-gray-50 hover:bg-gray-500 disabled:text-gray-300 disabled:opacity-100"
               onClick={() =>
                 setFilters({
                   ...filters,
@@ -88,124 +83,144 @@ export default function FiltersSidebar({
               }
             >
               Reset all
+              <Reset className="h-5 w-5" />
             </Button>
-            <div className="mt-4 grid grid-cols-3 gap-4">
-              <MultiCombobox
-                name="Source"
-                value={filters.source ?? []}
-                options={[
-                  { label: 'Cirad dataverse', value: DatasetSource.Cirad },
-                  { label: 'Harvard dataverse', value: DatasetSource.Harvard },
-                  { label: 'Inrae dataverse', value: DatasetSource.Inrae },
-                  { label: 'Joint Research Centre Data Catalogue', value: DatasetSource.JRC },
-                  { label: 'Zenodo', value: DatasetSource.Zenodo },
-                ]}
-                onChange={(value) => setFilters({ ...filters, source: value })}
-              />
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button
-                    variant="vanilla"
-                    size="auto"
-                    className="relative w-full justify-start border border-gray-300 p-4 pr-12 text-base focus-visible:!outline-1 focus-visible:!outline-offset-0 focus-visible:!outline-gray-300 focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-[3px] data-[state=open]:border-gray-400"
+            <div className="space-y-6">
+              <div>
+                <label htmlFor="source" className="text-sm text-gray-200">
+                  Sources
+                </label>
+                <MultiCombobox
+                  id="source"
+                  name="Sources"
+                  placeholder="Select"
+                  value={filters.source ?? []}
+                  options={[
+                    { label: 'Cirad dataverse', value: DatasetSource.Cirad },
+                    { label: 'Harvard dataverse', value: DatasetSource.Harvard },
+                    { label: 'Inrae dataverse', value: DatasetSource.Inrae },
+                    { label: 'Joint Research Centre Data Catalogue', value: DatasetSource.JRC },
+                    { label: 'Zenodo', value: DatasetSource.Zenodo },
+                  ]}
+                  onChange={(value) => setFilters({ ...filters, source: value })}
+                />
+              </div>
+              <div>
+                <label htmlFor="from-date" className="text-sm text-gray-200">
+                  From date
+                </label>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      size="auto"
+                      type="button"
+                      className="flex h-10 w-full justify-between border border-gray-300 bg-gray-700 px-4 py-2 text-left text-base hover:!bg-gray-700 focus-visible:!outline-1 focus-visible:!outline-offset-0 focus-visible:!outline-green-700 data-[state=open]:border-gray-400"
+                    >
+                      {filters.minDate ? (
+                        format({ id: 'formatDate', value: filters.minDate })
+                      ) : (
+                        <span>Select</span>
+                      )}
+                      <ChevronDown className="h-6 w-6 flex-shrink-0" />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent
+                    className="w-[330px] overflow-y-auto rounded-none border border-gray-400 p-0 text-base shadow-none"
+                    side="bottom"
+                    sideOffset={-1}
+                    align="start"
                   >
-                    {filters.minDate ? (
-                      format({ id: 'formatDate', value: filters.minDate })
-                    ) : (
-                      <span>From date</span>
-                    )}
-                    <ChevronDown className="absolute right-4 top-4 h-6 w-6 flex-shrink-0" />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent
-                  className="w-[330px] overflow-y-auto rounded-none border border-gray-400 p-0 text-base shadow-none"
-                  side="bottom"
-                  sideOffset={-1}
-                  align="start"
-                >
-                  <Calendar
-                    initialFocus
-                    variant="datasets"
-                    mode="single"
-                    captionLayout="dropdown-buttons"
-                    defaultMonth={filters.minDate ? new Date(filters.minDate) : undefined}
-                    // `fromYear` and `toDate` are required to allow the user to quickly jump
-                    // between years and months (see the `captionLayout` prop)
-                    fromYear={2000}
-                    toDate={
-                      filters.maxDate
-                        ? // We're making sure the user can't select the same date in both date
+                    <Calendar
+                      id="from-date"
+                      initialFocus
+                      variant="datasets"
+                      mode="single"
+                      captionLayout="dropdown-buttons"
+                      defaultMonth={filters.minDate ? new Date(filters.minDate) : undefined}
+                      // `fromYear` and `toDate` are required to allow the user to quickly jump
+                      // between years and months (see the `captionLayout` prop)
+                      fromYear={2000}
+                      toDate={
+                        filters.maxDate
+                          ? // We're making sure the user can't select the same date in both date
                           // pickers because the API considers the max date as exclusive
                           new Date(+new Date(filters.maxDate) - 24 * 3600 * 1000)
-                        : new Date()
-                    }
-                    selected={filters.minDate ? new Date(filters.minDate) : undefined}
-                    onSelect={(date) =>
-                      setFilters({
-                        ...filters,
-                        minDate: date
-                          ? `${date.getFullYear()}-${`${date.getMonth() + 1}`.padStart(
+                          : new Date()
+                      }
+                      selected={filters.minDate ? new Date(filters.minDate) : undefined}
+                      onSelect={(date) =>
+                        setFilters({
+                          ...filters,
+                          minDate: date
+                            ? `${date.getFullYear()}-${`${date.getMonth() + 1}`.padStart(
                               2,
                               '0',
                             )}-${`${date.getDate()}`.padStart(2, '0')}`
-                          : undefined,
-                      })
-                    }
-                  />
-                </PopoverContent>
-              </Popover>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button
-                    variant="vanilla"
-                    size="auto"
-                    className="relative w-full justify-start border border-gray-300 p-4 pr-12 text-base focus-visible:!outline-1 focus-visible:!outline-offset-0 focus-visible:!outline-gray-300 focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-[3px] data-[state=open]:border-gray-400"
+                            : undefined,
+                        })
+                      }
+                    />
+                  </PopoverContent>
+                </Popover>
+              </div>
+              <div>
+                <label htmlFor="to-date" className="text-sm text-gray-200">
+                  To date
+                </label>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      size="auto"
+                      type="button"
+                      className="flex h-10 w-full justify-between border border-gray-300 bg-gray-700 px-4 py-2 text-left text-base hover:!bg-gray-700 focus-visible:!outline-1 focus-visible:!outline-offset-0 focus-visible:!outline-green-700 data-[state=open]:border-gray-400"
+                    >
+                      {filters.maxDate ? (
+                        format({ id: 'formatDate', value: filters.maxDate })
+                      ) : (
+                        <span>Select</span>
+                      )}
+                      <ChevronDown className="h-6 w-6 flex-shrink-0" />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent
+                    className="w-[330px] overflow-y-auto rounded-none border border-gray-400 p-0 text-base shadow-none"
+                    side="bottom"
+                    sideOffset={-1}
+                    align="start"
                   >
-                    {filters.maxDate ? (
-                      format({ id: 'formatDate', value: filters.maxDate })
-                    ) : (
-                      <span>To date</span>
-                    )}
-                    <ChevronDown className="absolute right-4 top-4 h-6 w-6 flex-shrink-0" />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent
-                  className="w-[330px] overflow-y-auto rounded-none border border-gray-400 p-0 text-base shadow-none"
-                  side="bottom"
-                  sideOffset={-1}
-                  align="start"
-                >
-                  <Calendar
-                    initialFocus
-                    variant="datasets"
-                    mode="single"
-                    captionLayout="dropdown-buttons"
-                    defaultMonth={filters.maxDate ? new Date(filters.maxDate) : undefined}
-                    // `fromDate` and `toDate` are required to allow the user to quickly jump
-                    // between years and months (see the `captionLayout` prop)
-                    fromDate={
-                      filters.minDate
-                        ? // We're making sure the user can't select the same date in both date
+                    <Calendar
+                      id="to-date"
+                      initialFocus
+                      variant="datasets"
+                      mode="single"
+                      captionLayout="dropdown-buttons"
+                      defaultMonth={filters.maxDate ? new Date(filters.maxDate) : undefined}
+                      // `fromDate` and `toDate` are required to allow the user to quickly jump
+                      // between years and months (see the `captionLayout` prop)
+                      fromDate={
+                        filters.minDate
+                          ? // We're making sure the user can't select the same date in both date
                           // pickers because the API considers the max date as exclusive
                           new Date(+new Date(filters.minDate) + 24 * 3600 * 1000)
-                        : new Date('2000-01-01')
-                    }
-                    toDate={new Date()}
-                    selected={filters.maxDate ? new Date(filters.maxDate) : undefined}
-                    onSelect={(date) =>
-                      setFilters({
-                        ...filters,
-                        maxDate: date
-                          ? `${date.getFullYear()}-${`${date.getMonth() + 1}`.padStart(
+                          : new Date('2000-01-01')
+                      }
+                      toDate={new Date()}
+                      selected={filters.maxDate ? new Date(filters.maxDate) : undefined}
+                      onSelect={(date) =>
+                        setFilters({
+                          ...filters,
+                          maxDate: date
+                            ? `${date.getFullYear()}-${`${date.getMonth() + 1}`.padStart(
                               2,
                               '0',
                             )}-${`${date.getDate()}`.padStart(2, '0')}`
-                          : undefined,
-                      })
-                    }
-                  />
-                </PopoverContent>
-              </Popover>
+                            : undefined,
+                        })
+                      }
+                    />
+                  </PopoverContent>
+                </Popover>
+              </div>
             </div>
           </fieldset>
         </div>
--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -34,19 +34,23 @@ const calendarDayVariants = cva('text-white hover:text-white focus:text-white', 
 });
 
 export type CalendarProps = ComponentProps<typeof DayPicker> &
-  VariantProps<typeof calendarDayVariants>;
+  VariantProps<typeof calendarDayVariants> & {
+    id: string;
+  };
 
 function Calendar({
   className,
   classNames,
   showOutsideDays = true,
   variant,
+  id,
   ...props
 }: CalendarProps) {
   return (
     <DayPicker
+      id={id}
       showOutsideDays={showOutsideDays}
-      className={cn('p-6', className)}
+      className={cn('p-6 text-gray-700', className)}
       classNames={{
         months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
         month: 'space-y-4 w-full',
@@ -55,7 +59,7 @@ function Calendar({
         caption_dropdowns: 'flex gap-x-4',
         vhidden: 'hidden',
         nav: 'space-x-1 flex items-center',
-        nav_button: buttonVariants({ size: 'icon' }),
+        nav_button: cn(buttonVariants({ variant: 'secondary', size: 'icon' })),
         nav_button_previous: 'absolute left-1',
         nav_button_next: 'absolute right-1',
         table: 'mx-auto border-collapse space-y-1',
@@ -65,16 +69,17 @@ function Calendar({
         cell: 'h-9 w-10 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
         day: cn(
           buttonVariants({ variant: 'ghost' }),
-          'h-9 w-10 p-0 font-semibold aria-selected:opacity-100 cursor-pointer',
+          'h-9 w-10 p-0 font-semibold aria-selected:opacity-100 cursor-pointer text-white',
         ),
         day_range_end: 'day-range-end',
         day_selected: calendarDayVariants({ variant }),
-        day_today: 'bg-accent text-accent-foreground',
+        day_today: 'bg-gray-600',
         day_outside:
           'day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30',
         day_disabled: 'text-muted-foreground opacity-50',
         day_range_middle: 'aria-selected:bg-accent aria-selected:text-accent-foreground',
         day_hidden: 'invisible',
+        root: 'rounded-lg border-none bg-gray-650',
         ...classNames,
       }}
       components={{
@@ -133,7 +138,7 @@ function Calendar({
         },
         CaptionLabel: (captionLabelProps) =>
           props.captionLayout === 'dropdown' ||
-          props.captionLayout === 'dropdown-buttons' ? null : (
+            props.captionLayout === 'dropdown-buttons' ? null : (
             <CaptionLabel {...captionLabelProps} />
           ),
       }}
