9bea7af8e134ad7efc071be6a062cc350fe9e7c7
Clément Prod'homme <contact@clementprodhomme.fr>
2024-04-16 11:48:02 +0200

    Simplify the map settings (labels & boundaries)

--- client/src/containers/map/settings/labels/index.tsx
+++ client/src/containers/map/settings/labels/index.tsx
@@ -2,62 +2,28 @@ import { useCallback } from 'react';
 
 import { useMapSettings } from '@/store/index';
 
-import { LABELS } from '@/constants/basemaps';
-
 import { Label } from '@/components/ui/label';
-import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Switch } from '@/components/ui/switch';
 
 const Labels = () => {
   const [{ labels }, setMapSettings] = useMapSettings();
 
-  const onChange = useCallback(
-    (labelsSlug: string) => {
-      setMapSettings((prev) => ({
-        ...prev,
-        labels: labelsSlug,
-      }));
-    },
-    [setMapSettings],
-  );
-
   const onToggle = useCallback(
     (toggled: boolean) => {
       setMapSettings((prev) => ({
         ...prev,
-        labels: toggled ? LABELS[0].slug : null,
+        labels: toggled,
       }));
     },
     [setMapSettings],
   );
 
   return (
-    <div className="space-y-4">
-      <div className="flex w-full items-center justify-between border-b border-gray-150 pb-1">
-        <Label htmlFor="labels-switch" className="font-serif text-lg">
-          Labels
-        </Label>
-        <Switch id="labels-switch" checked={labels !== null} onCheckedChange={onToggle} />
-      </div>
-      <RadioGroup
-        value={labels ?? LABELS[0].slug}
-        onValueChange={onChange}
-        disabled={labels === null}
-        className="flex gap-4"
-      >
-        {LABELS.map(({ label, slug }) => (
-          <div key={slug} className="group flex cursor-pointer items-center space-x-2">
-            <RadioGroupItem value={slug} id={`labels-${slug}`} />
-            <Label
-              variant={labels === null ? 'disabled' : 'default'}
-              className="text-base"
-              htmlFor={`labels-${slug}`}
-            >
-              {label}
-            </Label>
-          </div>
-        ))}
-      </RadioGroup>
+    <div className="flex w-full items-center justify-between">
+      <Label htmlFor="labels-switch" className="font-serif text-lg">
+        Labels
+      </Label>
+      <Switch id="labels-switch" checked={labels} onCheckedChange={onToggle} />
     </div>
   );
 };
