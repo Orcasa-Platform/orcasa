59fc107ac8a13e0eff5ee64e6c7f66201814bb6f
Alvaro <pursuance@gmail.com>
2024-05-23 09:14:41 +0200

    Add opt-out div and script. Style it a bit (#313)

--- client/src/app/(home)/(static-pages)/personal-data-protection/page.tsx
+++ client/src/app/(home)/(static-pages)/personal-data-protection/page.tsx
@@ -1,9 +1,17 @@
+import Script from 'next/script';
+
 import { Metadata } from 'next';
 
 import StaticPageContent from '@/components/home/static-page-content';
 
 export default async function PersonalDataProtection() {
-  return <StaticPageContent slug="personal-data-protection" />;
+  return (
+    <>
+      <StaticPageContent slug="personal-data-protection" />
+      <div id="matomo-opt-out" className="my-4 [&>*]:font-sans [&>input]:mr-2"></div>
+      <Script src="https://matomo.cirad.fr/index.php?module=CoreAdminHome&action=optOutJS&divId=matomo-opt-out&language=en&fontSize=16px&showIntro=0" />
+    </>
+  );
 }
 
 export const metadata: Metadata = {
