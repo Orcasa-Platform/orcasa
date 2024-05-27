8cbed861d682347c523c3612d880ea3601a3f38c
Alvaro Leal <pursuance@gmail.com>
2024-05-27 13:45:50 +0200

    Use server side function to get git info

--- client/src/app/api/log-info/route.ts
+++ client/src/app/api/log-info/route.ts
@@ -1,3 +1,18 @@
+// utils/gitUtils.js
+import { execSync } from 'child_process';
+
+export function getLastCommitInfo() {
+  try {
+    // Fetch the last commit SHA and message
+    const commitInfo = execSync('git log -1 --format=%H%n%B').toString().trim();
+    const [sha, message] = commitInfo.split('\n');
+    return { sha, message };
+  } catch (error) {
+    console.error('Failed to get last commit info:', error);
+    throw error;
+  }
+}
+
 import { NextResponse } from 'next/server';
 export async function GET() {
   const publicVarKeys = Object.keys(process.env).filter((key) => key.startsWith('NEXT_PUBLIC'));
@@ -6,11 +21,19 @@ export async function GET() {
     return acc;
   }, {} as Record<string, string>);
 
-  return NextResponse.json({
-    lastCommit: {
-      message: process.env.VERCEL_GIT_COMMIT_MESSAGE,
-      SHA: process.env.VERCEL_GIT_COMMIT_SHA,
-    },
-    vars: publicVars,
-  });
+  try {
+    // Find the last commit message and SHA
+    const { sha, message } = getLastCommitInfo();
+
+    return NextResponse.json({
+      lastCommit: {
+        sha,
+        message,
+      },
+      vars: publicVars,
+    });
+  } catch (error) {
+    console.error('Failed to get info');
+    return NextResponse.error();
+  }
 }
