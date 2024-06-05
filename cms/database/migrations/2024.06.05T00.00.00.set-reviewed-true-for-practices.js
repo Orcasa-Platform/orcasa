d32362b8e44d99b8a96e1f18055708899ed60b8c
Clément Prod'homme <contact@clementprodhomme.fr>
2024-06-05 10:41:21 +0200

    Create the `reviewed` attribute in the migration

--- cms/database/migrations/2024.06.05T00.00.00.set-reviewed-true-for-practices.js
+++ cms/database/migrations/2024.06.05T00.00.00.set-reviewed-true-for-practices.js
@@ -1,6 +1,12 @@
 'use strict'
 
 async function up(knex) {
+  // Create the `reviewed` column as the migration is run before the table has been updated
+  await knex.schema.alterTable('practices', (table) => {
+    table.boolean('reviewed').defaultTo(false);
+  });
+
+  // Set the value to `true` for all the existing rows
   await knex.from('practices').update({ reviewed: true });
 }
 
