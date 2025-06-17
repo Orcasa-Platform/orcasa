Orcasa-Platform/orcasa

commit 2f9b35791ae1695d85423bfbad97f2b468808f59

fix(network): Prevent suggesting already existing but not yet approved organisation/initiative

cms/src/api/organization/controllers/organization.ts
@@ -25,7 +25,15 @@ export default factories.createCoreController('api::organization.organization', 
   },
 
   async find(ctx) {
-    ctx.query.filters = { ...ctx.query.filters, publication_status: { $eq: 'accepted' } }
+    // Only allow to search for the organizations with the proposed status if we don't retrieve their attributes. This
+    // can be useful to determine if one already exists with a specific name.
+    // It is not totally safe though as the filters can be used to guess the attributes by brute-force.
+    const canSearchProposedStatus = ctx.query.fields.length === 1 && ctx.query.fields[0] === 'id';
+
+    ctx.query.filters = {
+      ...ctx.query.filters,
+      ...(canSearchProposedStatus ? {} : { publication_status: { $eq: 'accepted' } }),
+    };
 
     return await super.find(ctx);
   },

cms/src/api/project/controllers/project.ts
@@ -25,7 +25,15 @@ export default factories.createCoreController('api::project.project', () => ({
   },
 
   async find(ctx) {
-    ctx.query.filters = { ...ctx.query.filters, publication_status: { $eq: 'accepted' } }
+    // Only allow to search for the initiatives with the proposed status if we don't retrieve their attributes. This
+    // can be useful to determine if one already exists with a specific name.
+    // It is not totally safe though as the filters can be used to guess the attributes by brute-force.
+    const canSearchProposedStatus = ctx.query.fields.length === 1 && ctx.query.fields[0] === 'id';
+
+    ctx.query.filters = {
+      ...ctx.query.filters,
+      ...(canSearchProposedStatus ? {} : { publication_status: { $eq: 'accepted' } }),
+    };
 
     return await super.find(ctx);
   },

commit dcf8457ab39ada21acbde1066cbd0b67b32a3153

fix(network): Regression from 2f9b35791ae1695d85423bfbad97f2b468808f59

cms/src/api/organization/controllers/organization.ts
@@ -28,7 +28,8 @@ export default factories.createCoreController('api::organization.organization', 
     // Only allow to search for the organizations with the proposed status if we don't retrieve their attributes. This
     // can be useful to determine if one already exists with a specific name.
     // It is not totally safe though as the filters can be used to guess the attributes by brute-force.
-    const canSearchProposedStatus = ctx.query.fields.length === 1 && ctx.query.fields[0] === 'id';
+    const canSearchProposedStatus = ctx.query.fields !== undefined && ctx.query.fields !== null
+      && ctx.query.fields.length === 1 && ctx.query.fields[0] === 'id';
 
     ctx.query.filters = {
       ...ctx.query.filters,

cms/src/api/project/controllers/project.ts
@@ -28,7 +28,8 @@ export default factories.createCoreController('api::project.project', () => ({
     // Only allow to search for the initiatives with the proposed status if we don't retrieve their attributes. This
     // can be useful to determine if one already exists with a specific name.
     // It is not totally safe though as the filters can be used to guess the attributes by brute-force.
-    const canSearchProposedStatus = ctx.query.fields.length === 1 && ctx.query.fields[0] === 'id';
+    const canSearchProposedStatus = ctx.query.fields !== undefined && ctx.query.fields !== null
+      && ctx.query.fields.length === 1 && ctx.query.fields[0] === 'id';
 
     ctx.query.filters = {
       ...ctx.query.filters,